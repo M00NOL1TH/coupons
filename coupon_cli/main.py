ef3abba7511e800825549ead42c383cf2e2c9614
Andras Hegyi <hegyi.andras@hotmail.com>
2023-06-02 06:10:38 +0200

    Add some data to demo fixture

--- coupon_cli/main.py
+++ coupon_cli/main.py
@@ -1,11 +1,15 @@
+from datetime import datetime, timedelta
+import random
+import string
+
 from sqlmodel import Session, SQLModel
 from typer import Typer
 
 from coupon_app.main import get_db_engine
 from coupon_app.settings import get_settings
 from coupon_model import init_models  # noqa
-from coupon_model.coupon.model import CouponTable
-from coupon_model.customer.model import CustomerTable
+from coupon_model.coupon.model import CouponTable, CouponCreate, DiscountType
+from coupon_model.customer.model import CustomerTable, CustomerCreate
 
 
 app = Typer()
@@ -37,11 +41,50 @@ def demo_fixture():
     # Initialize the database from SQLModel's metadata.
     SQLModel.metadata.create_all(engine)
 
+    now = datetime.utcnow()
+
     with Session(engine) as session:
         print("Clear database")
+
         for table in (CouponTable, CustomerTable):
             session.query(table).delete()
             session.commit()
+
+        print("Create customers")
+
+        session.add_all(
+            [
+                CustomerTable.from_orm(
+                    CustomerCreate(
+                        name=f"Customer {i}",
+                        username=f"customer{i}",
+                    )
+                )
+                for i in range(10)
+            ]
+        )
+        session.commit()
+
+        print("Create coupons")
+
+        session.add_all(
+            [
+                CouponTable.from_orm(
+                    CouponCreate(
+                        code="".join(random.choice(string.ascii_uppercase) for i in range(8)),
+                        description="The greatest discount",
+                        discount=42,
+                        discount_type=DiscountType.fixed if i % 3 == 1 else DiscountType.percentage,
+                        is_active=True if i % 2 == 1 else False,
+                        valid_from=now + timedelta(days=random.randrange(-7, 7)),
+                        valid_until=now + timedelta(days=random.randrange(14, 28)),
+                    )
+                )
+                for i in range(20)
+            ]
+        )
+
+        session.commit()
 
 
 if __name__ == "__main__":
